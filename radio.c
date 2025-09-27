6417e19ab61fc5a05d005d592d2c3369d02df55c
Phil <feulouque@gmail.com>
2025-09-27 22:01:37 +0200

    v2.1x16

--- radio.c
+++ radio.c
@@ -706,14 +706,14 @@ void RADIO_SetupRegisters(bool switchToForeground)
 	#endif
 	BK4819_SetFrequency(Frequency);
 
-/*
+
 
 	BK4819_SetupSquelch(
 		gRxVfo->SquelchOpenRSSIThresh,    gRxVfo->SquelchCloseRSSIThresh,
 		gRxVfo->SquelchOpenNoiseThresh,   gRxVfo->SquelchCloseNoiseThresh,
 		gRxVfo->SquelchCloseGlitchThresh, gRxVfo->SquelchOpenGlitchThresh);
 
-	BK4819_PickRXFilterPathBasedOnFrequency(Frequency); */
+	BK4819_PickRXFilterPathBasedOnFrequency(Frequency); 
 
 	// what does this in do ?
 	BK4819_ToggleGpioOut(BK4819_GPIO0_PIN28_RX_ENABLE, true); 
